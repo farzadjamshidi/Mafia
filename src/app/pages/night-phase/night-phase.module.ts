d704693c127233b8538fd7d335545da8a5b6a9b5
farzadjamshidi <farzadjamshidicom@yahoo.com>
2022-07-09 19:59:32 +0300

    Add confirmation dialog to night phase

--- src/app/pages/night-phase/night-phase.module.ts
+++ src/app/pages/night-phase/night-phase.module.ts
@@ -7,6 +7,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatRadioModule } from '@angular/material/radio';
 import { MatSelectModule } from '@angular/material/select';
 import { TranslateModule } from '@ngx-translate/core';
+import { ConfirmationDialogModule } from 'src/app/components/confirmation-dialog/confirmation-dialog.module';
 import { LocalStorageV0PlayerRepo } from 'src/app/core/repository/localStorage/v0/player.repo';
 import { NightPhaseRoutingModule } from './night-phase-routing.module';
 import { NightPhaseComponent } from './night-phase.component';
@@ -25,16 +26,20 @@ const MATERIAL_MODULES = [
   MatButtonModule,
   MatCardModule
 ];
-
+const MAFIA_MODULES = [
+  ConfirmationDialogModule
+];
 @NgModule({
   declarations: [
     ...COMPONENTS
   ],
   imports: [
+    ...MAFIA_MODULES,
     ...MATERIAL_MODULES,
     ...BASE_MODULES
   ],
   exports: [
+    ...MAFIA_MODULES,
     ...MATERIAL_MODULES,
     ...COMPONENTS
   ],
