73199233d7d4f4ce3920194b642b3a3932c05819
farzadjamshidi <farzadjamshidicom@yahoo.com>
2022-07-09 19:25:43 +0300

    Add confirm dialog to show roles

--- src/app/pages/show-roles/show-roles.component.ts
+++ src/app/pages/show-roles/show-roles.component.ts
@@ -1,7 +1,11 @@
 import { Component, Inject, OnDestroy, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { MatSelectChange } from '@angular/material/select';
 import { Router } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
 import { Subscription } from 'rxjs';
+import { ConfirmationDialogComponent } from 'src/app/components/confirmation-dialog/confirmation-dialog.component';
+import { ConfirmationDialogDataModel } from 'src/app/components/confirmation-dialog/confirmation-dialog.model';
 import { GetPlayersRequest } from 'src/app/core/models/get-players.model';
 import { Player, PlayerRole } from 'src/app/core/models/player.model';
 import { IPlayerRepo } from 'src/app/core/repository/interfaces/player.interface';
@@ -20,6 +24,8 @@ export class ShowRolesComponent implements OnInit, OnDestroy
   selectedPlayer: Player = new Player();
 
   constructor(
+    public dialog: MatDialog,
+    private translate: TranslateService,
     private router: Router,
     @Inject('IPlayerRepo') private playerRepo: IPlayerRepo
   )
@@ -59,6 +65,25 @@ export class ShowRolesComponent implements OnInit, OnDestroy
 
   changePlayerComboBox(event: MatSelectChange): void
   {
+    this.selectedPlayer = new Player();
+
+    const data = new ConfirmationDialogDataModel({
+      title: 'SHOW_ROLES.TITLE',
+      content: this.translate.instant('SHOW_ROLES.DIALOG_CONTENT', { playerName: event.value.name })
+    });
+
+    const dialogRef = this.dialog.open(ConfirmationDialogComponent, {
+      width: '300px',
+      data
+    });
+
+    this.subscriptions.add(
+      dialogRef.afterClosed().subscribe((result: boolean) =>
+      {
+        if (result)
+          this.selectedPlayer = event.value;
+      })
+    );
   }
 
   actionFinished(): void
